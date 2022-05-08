5bfcef63383c01205994b2e200cb99f2060b5239
feitaN17 <rocker1337pro@gmail.com>
2022-05-08 18:20:04 +0500

    full 14 dz

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,58 @@
 $(function () {
-  var mixer = mixitup(".cards");
-});
+  $(window).scroll(function () {
+    var target = $(this).scrollTop()
 
-const swiper = new Swiper(".swiper", {
+    if (target == 0) {
+      $('.header__menu').removeClass('header__menu--fixed')
+      $('.menu__btn').css('top', '30px')
+    } else {
+      $('.header__menu').addClass('header__menu--fixed')
+      $('.menu__btn').css('top', '20px')
+    }
+  })
+
+  $('.menu__btn, .menu__btn2').click(function () {
+    $(this).toggleClass('menu__btn--open')
+    $('.menu__inner').toggleClass('menu__inner--open')
+    $('.wrapper').toggleClass('wrapper--fixed')
+  })
+  $('.menu__link, .logo').on('click', function () {
+    $('.menu__inner, .menu__btn').removeClass('menu__btn--open , menu__inner--open')
+    $('.wrapper').removeClass('wrapper--fixed')
+  })
+
+  const restaurantsSwiper = new Swiper('.restaurants-swiper', {
+    effect: 'coverflow',
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: 'true',
+    },
+  })
+
+  var mixer = mixitup('.popular-catigories__cards')
+})
+
+const swiper2 = new Swiper('.reviews-swiper', {
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
-    clickable: "true",
+    el: '.swiper-pagination',
+    clickable: 'true',
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
-});
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,95 @@
-const { src, dest, watch, parallel, series } = require("gulp");
+const { src, dest, watch, parallel, series } = require('gulp')
 
-const fileInclude = require("gulp-file-include");
-const scss = require("gulp-sass")(require("sass"));
-const svgSprite = require("gulp-svg-sprite");
-const cheerio = require("gulp-cheerio");
-const replace = require("gulp-replace");
-const concat = require("gulp-concat");
-const autoprefixer = require("gulp-autoprefixer");
-const uglify = require("gulp-uglify");
-const imagemin = require("gulp-imagemin");
-const del = require("del");
-const browserSync = require("browser-sync").create();
+const fileInclude = require('gulp-file-include')
+const scss = require('gulp-sass')(require('sass'))
+const svgSprite = require('gulp-svg-sprite')
+const cheerio = require('gulp-cheerio')
+const replace = require('gulp-replace')
+const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
+const uglify = require('gulp-uglify')
+const imagemin = require('gulp-imagemin')
+const del = require('del')
+const browserSync = require('browser-sync').create()
 
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: "app/",
+      baseDir: 'app/',
     },
     notify: false,
-  });
+  })
 }
 
 const htmlInclude = () => {
-  return src(["app/html/*.html"]) // Находит любой .html файл в папке "html", куда будем подключать другие .html файлы
+  return src(['app/html/*.html']) // Находит любой .html файл в папке "html", куда будем подключать другие .html файлы
     .pipe(
       fileInclude({
-        prefix: "@",
-        basepath: "@file",
+        prefix: '@',
+        basepath: '@file',
       })
     )
-    .pipe(dest("app")) // указываем, в какую папку поместить готовый файл html
-    .pipe(browserSync.stream());
-};
+    .pipe(dest('app')) // указываем, в какую папку поместить готовый файл html
+    .pipe(browserSync.stream())
+}
 
 function styles() {
-  return src("app/scss/style.scss")
-    .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
-    .pipe(concat("style.min.css"))
+  return src('app/scss/style.scss')
+    .pipe(scss({ outputStyle: 'compressed' }).on('error', scss.logError))
+    .pipe(concat('style.min.css'))
     .pipe(
       autoprefixer({
-        overrideBrowserslist: ["last 10 versions"],
-        grid: true,
+        overrideBrowserslist: ['last 10 versions'],
+        grid: 'true',
       })
     )
-    .pipe(dest("app/css"))
-    .pipe(browserSync.stream());
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
 function scripts() {
   return src([
-    "node_modules/jquery/dist/jquery.js",
-    "node_modules/mixitup/dist/mixitup.min.js",
-    "node_modules/swiper/swiper-bundle.min.js",
-    "app/js/main.js",
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/mixitup/dist/mixitup.min.js',
+    'node_modules/swiper/swiper-bundle.min.js',
+    'app/js/main.js',
   ])
-    .pipe(concat("main.min.js"))
+    .pipe(concat('main.min.js'))
     .pipe(uglify())
-    .pipe(dest("app/js"))
-    .pipe(browserSync.stream());
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
 function svgSprites() {
-  return (
-    src("app/images/icons/*.svg") // выбираем в папке с иконками все файлы с расширением svg
-      // .pipe(
-      //   cheerio({
-      //     run: ($) => {
-      //       $("[fill]").removeAttr("fill"); // очищаем цвет у иконок по умолчанию, чтобы можно было задать свой
-      //       $("[stroke]").removeAttr("stroke"); // очищаем, если есть лишние атрибуты строк
-      //       $("[style]").removeAttr("style"); // убираем внутренние стили для иконок
-      //     },
-      //     parserOptions: { xmlMode: true },
-      //   })
-      // )
-      // .pipe(replace("&gt;", ">")) // боремся с заменой символа
-      .pipe(
-        svgSprite({
-          mode: {
-            stack: {
-              sprite: "../sprite.svg", // указываем имя файла спрайта и путь
-            },
+  return src('app/img/icons/**.svg')
+    .pipe(
+      cheerio({
+        run: ($) => {
+          $('[fill]').removeAttr('fill')
+          $('[stroke]').removeAttr('stroke')
+          $('[style]').removeAttr('style')
+        },
+        parserOptions: {
+          xmlMode: true,
+        },
+      })
+    )
+    .pipe(replace('&gt;', '>'))
+
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg', // указываем имя файла спрайта и путь
           },
-        })
-      )
-      .pipe(dest("app/images"))
-  ); // указываем, в какую папку поместить готовый файл спрайта
+        },
+      })
+    )
+    .pipe(dest('app/img')) // указываем, в какую папку поместить готовый файл спрайта
 }
 
-function images() {
-  return src("app/images/**/*.*")
+function img() {
+  return src('app/img/**/*.*')
     .pipe(
       imagemin([
         imagemin.gifsicle({ interlaced: true }),
@@ -99,42 +100,35 @@ function images() {
         }),
       ])
     )
-    .pipe(dest("dist/images"));
+    .pipe(dest('dist/img'))
 }
 
 function build() {
-  return src(["app/**/*.html", "app/css/style.min.css", "app/js/main.min.js"], {
-    base: "app",
-  }).pipe(dest("dist"));
+  return src(['app/**/*.html', 'app/css/style.min.css', 'app/js/main.min.js'], {
+    base: 'app',
+  }).pipe(dest('dist'))
 }
 
 function clean() {
-  return del("dist");
+  return del('dist')
 }
 
 function watching() {
-  watch(["app/html/**/*.html"], htmlInclude);
-  watch(["app/scss/**/*.scss"], styles);
-  watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
-  watch(["app/images/icons/*.svg"], svgSprites);
-  watch(["app/**/*.html"]).on("change", browserSync.reload);
+  watch(['app/html/**/*.html'], htmlInclude)
+  watch(['app/scss/**/*.scss'], styles)
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
+  watch(['app/img/icons/*.svg'], svgSprites)
+  watch(['app/**/*.html']).on('change', browserSync.reload)
 }
 
-exports.htmlInclude = htmlInclude;
-exports.styles = styles;
-exports.scripts = scripts;
-exports.svgSprites = svgSprites;
-exports.browsersync = browsersync;
-exports.watching = watching;
-exports.images = images;
-exports.clean = clean;
-exports.build = series(clean, images, build);
+exports.htmlInclude = htmlInclude
+exports.styles = styles
+exports.scripts = scripts
+exports.svgSprites = svgSprites
+exports.browsersync = browsersync
+exports.watching = watching
+exports.img = img
+exports.clean = clean
+exports.build = series(clean, img, build)
 
-exports.default = parallel(
-  svgSprites,
-  htmlInclude,
-  styles,
-  scripts,
-  browsersync,
-  watching
-);
+exports.default = parallel(svgSprites, htmlInclude, styles, scripts, browsersync, watching)
